70a6044a394dfc67584bbe33db8787693b3c8553
unknown <pratheeba.sekaran@gmail.com>
2020-09-30 09:35:29 +0530

    finall commit

---
+++ build/precache-manifest.2721ba1ea810949166316c0a13b44d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd468aa3d0a961d35ca4aec07b69a73b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9af69bfe495eb6fd6119",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "c9c0a47381569f0ec9e3",
+    "url": "/static/js/2.94454873.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.94454873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af69bfe495eb6fd6119",
+    "url": "/static/js/main.e4770f91.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
